4a065acffe50023debf7d730764ad92d8fe694d1
Jay <me@jayxtq.xyz>
2024-11-22 16:26:02 +0000

    chore: change versions

--- DiscordLab.Moderation/Plugin.cs
+++ DiscordLab.Moderation/Plugin.cs
@@ -11,7 +11,7 @@ namespace DiscordLab.Moderation
         public override string Name => "DiscordLab.Moderation";
         public override string Author => "JayXTQ";
         public override string Prefix => "DL.Moderation";
-        public override Version Version => new (1, 2, 0);
+        public override Version Version => new (1, 0, 0);
         public override Version RequiredExiledVersion => new (8, 11, 0);
         public override PluginPriority Priority => PluginPriority.Default;
 
