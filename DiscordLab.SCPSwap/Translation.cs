1344319f98729d51c3bedd0d368cf0b0d64387c4
Jay <me@jayxtq.xyz>
2024-11-30 22:38:56 +0000

    style(SCPSwap): better translation

--- DiscordLab.SCPSwap/Translation.cs
+++ DiscordLab.SCPSwap/Translation.cs
@@ -4,6 +4,6 @@ namespace DiscordLab.SCPSwap
 {
     public class Translation : ITranslation
     {
-        public string Message { get; set; } = "Player {player} ({playerid}) has swapped from {oldrole} to {newrole}.";
+        public string Message { get; set; } = "Player `{player}` (`{playerid}`) has swapped from `{oldrole}` to `{newrole}`.";
     }
 }
