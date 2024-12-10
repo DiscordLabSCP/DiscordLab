a2522ffe6ae249d8689f4752c90d5b4ea84afc2b
Jay <me@jayxtq.xyz>
2024-12-10 17:54:04 +0000

    chore(ML): boost version

--- DiscordLab.ModerationLogs/Plugin.cs
+++ DiscordLab.ModerationLogs/Plugin.cs
@@ -11,7 +11,7 @@ namespace DiscordLab.ModerationLogs
         public override string Name => "DiscordLab.ModerationLogs";
         public override string Author => "JayXTQ";
         public override string Prefix => "DL.ModerationLogs";
-        public override Version Version => new (1, 4, 0);
+        public override Version Version => new (1, 4, 1);
         public override Version RequiredExiledVersion => new (8, 11, 0);
         public override PluginPriority Priority => PluginPriority.Default;
 
