b8015699fba8a32b728ae023c0f6ce65a521fa1c
Lumi <me@jxtq.moe>
2025-01-27 11:06:16 +0000

    style(RL): change player placeholder to playernickname

--- DiscordLab.RoundLogs/Translation.cs
+++ DiscordLab.RoundLogs/Translation.cs
@@ -7,8 +7,8 @@ namespace DiscordLab.RoundLogs
         public string RoundStartMessage { get; set; } = "Round has started! Started {timer}";
         public string RoundEndMessage { get; set; } = "Round has ended! Team {team} has won!";
         
-        public string PlayerCuffedMessage { get; set; } = "{player} has been cuffed by {issuer}!";
-        public string PlayerUncuffedMessage { get; set; } = "{player} has been uncuffed by {issuer}!";
+        public string PlayerCuffedMessage { get; set; } = "{playernickname} has been cuffed by {issuernickname}!";
+        public string PlayerUncuffedMessage { get; set; } = "{playernickname} has been uncuffed by {issuernickname}!";
         
         public string ChaosSpawnMessage { get; set; } = "Chaos Insurgency has spawned! {timer}";
         public string NtfSpawnMessage { get; set; } = "Nine-Tailed Fox ({unitname}-{unitnumber}) has spawned! {timer}";
