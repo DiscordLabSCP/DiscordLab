48ab3a894aab3f6f79e269053123fdf2656d7c05
Lumi <me@jxtq.moe>
2025-03-05 09:55:51 +0000

    feat: channel extensions

---
+++ DiscordLab.Bot/API/Extensions/ChannelExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordLab.Bot.API.Extensions
+{
+    public static class ChannelExtensions
+    {
+        public static void SendMessageSync(this SocketTextChannel channel,
+            string text = null, 
+            // ReSharper disable once InconsistentNaming
+            bool isTTS = false, 
+            Embed embed = null, 
+            RequestOptions options = null, 
+            AllowedMentions allowedMentions = null, 
+            MessageReference messageReference = null, 
+            MessageComponent components = null, 
+            ISticker[] stickers = null, 
+            Embed[] embeds = null, 
+            MessageFlags flags = MessageFlags.None, 
+            PollProperties poll = null
+        )
+        {
+            Task.Run(async () => 
+                await channel.SendMessageAsync(
+                    text,
+                    isTTS,
+                    embed,
+                    options,
+                    allowedMentions, 
+                    messageReference, 
+                    components, 
+                    stickers, 
+                    embeds, 
+                    flags, 
+                    poll
+                )
+            );
+        }
+    }
+}
