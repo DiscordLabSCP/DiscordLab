9d7044a017227e809b60c7cadb34a9c3362c7d01
Lumi <me@jxtq.moe>
2025-04-05 20:12:34 +0100

    feat: date time extensions

---
+++ DiscordLab.Bot/API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+
+namespace DiscordLab.Bot.API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToDiscordUnixTimestamp(this DateTime dateTime, string suffix = "")
+        {
+            if (suffix != "") return $"<t:{dateTime.ToUnixTimestamp()}:{suffix}>";
+            return $"<t:{dateTime.ToUnixTimestamp()}>";
+        }
+
+        public static long ToUnixTimestamp(this DateTime dateTime)
+        {
+            return new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+        }
+    }
+}
