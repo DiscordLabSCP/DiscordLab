8a3df7592f0e1e63cc6606573c3eac3def8d2ccc
Lumi <me@jxtq.moe>
2025-05-12 22:04:53 +0100

    feat: abstract slash command builder

---
+++ DiscordLab.Bot/API/Features/SlashCommand.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.WebSocket;
+using DiscordLab.Bot.API.Interfaces;
+
+namespace DiscordLab.Bot.API.Features
+{
+    public abstract class SlashCommand : IAutocompleteCommand
+    {
+        public abstract SlashCommandBuilder Data { get; }
+
+        public virtual ulong GuildId { get; set; } = 0;
+        
+        public abstract Task Run(SocketSlashCommand command);
+
+        public virtual Task Autocomplete(SocketAutocompleteInteraction autocomplete)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
