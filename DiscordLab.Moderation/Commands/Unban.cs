b8f0140ee4b9ccf9fa3e7a3ff151d648abfa0ca6
Jay <me@jxtq.moe>
2025-05-20 08:26:00 +0100

    chore: me when i forget i know basic security
    default permissions are there bruh

--- DiscordLab.Moderation/Commands/Unban.cs
+++ DiscordLab.Moderation/Commands/Unban.cs
@@ -34,12 +34,6 @@ namespace DiscordLab.Moderation.Commands
         {
             await command.DeferAsync(true);
             
-            if (command.User is not SocketGuildUser guildUser || !guildUser.Roles.Any(role => role.Id == Plugin.Instance.Config.UnbanCommandRole))
-            {
-                await command.ModifyOriginalResponseAsync(m => m.Content = Translation.NoPermissions);
-                return;
-            }
-            
             string user = command.Data.Options.First(option => option.Name == Translation.BanCommandUserOptionName)
                 .Value.ToString();
 
